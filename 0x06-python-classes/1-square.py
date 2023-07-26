6c0676fa609baf401980951d695361638237a28a
Fikayomi31 <fikayoogundijo@gmail.com>
2023-07-26 17:08:14 +0000

    task 1 with 1-main.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
 """Defining a class"""
+
+
 class Square:
 
-
+    def __init__(self, size):
+        """Initiazing of a class
+        Args:
+            size: size of the square
+            """
+        self.__size = size
