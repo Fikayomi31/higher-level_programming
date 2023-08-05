b2c476773a386c9ab365d576bb74f498f7425e79
Fikayomi31 <fikayoogundijo@gmail.com>
2023-08-05 10:21:28 +0000

    task 5, two combination number

--- 0x06-python-classes/item.py
+++ 0x06-python-classes/item.py
@@ -1,17 +1,36 @@
-class Item:
+import csv
 
+class Item:
+    pay_rate = 0.8 #The pay rate after 20%
+    all = []
     def __init__(self, name: str, price: float, quantity=0):
         self.name = name
         self.price = price
         self.quantity = quantity
+        
+        Item.all.append(self)
 
     def calculate_total_price(self):
         return self.price * self.quantity
 
+    def apply_discount(self):
+        self.price = self.price * Item.pay_rate
+    
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
 
-item1 = Item("phone", '100', 1)
-item2 = Item("Laptop", '1000', 3)
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=int(item.get('price')),
+                quantity=int(item.get('quantity')),
+            )
 
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
+Item.instantiate_from_csv()
+print(Item.all)
