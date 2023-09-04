ae3113291c56a9adfc4d01e15f4daf6d54d274ec
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-04 20:49:23 +0000

    task 0 - task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if  idx >= 0 or idx < len(my_list):
+        new_list[idx] = element
+    return new_list
+
+    
