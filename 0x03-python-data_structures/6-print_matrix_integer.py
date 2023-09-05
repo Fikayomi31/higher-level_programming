b12c76dcb6804353823f39da139fa5fce69ed5e1
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-05 22:42:33 +0000

    task 5-8

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            if col != row[-1]:
+                print("{}".format(col), end=' ')
+            else:
+                print("{}".format(col), end=' ')
+        print()
