3f2ed343303d5129edd1492866be1bdc5124e535
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-10 13:38:16 -0700

    update 1-main

--- 0x04-python-more_data_structures/1-main.py
+++ 0x04-python-more_data_structures/1-main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 search_replace = __import__("1-search_replace").search_replace
 
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89, 2, 2]
 new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
+print()
+my_list = [1, 4, 2, 4, 5, 4, 0, 4, 6,7, 4]
+new_list = search_replace(my_list, 4, 67)
 
 print(new_list)
 print(my_list)
