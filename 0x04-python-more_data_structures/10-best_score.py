889f1926fe73b673b30b23e6794a4b1c7f9307be
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-13 16:33:48 +0000

    task 10- 11

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    max_key = max(a_dictionary, key=a_dictionary.get)
-    max_value = a_dictionary[max_key]
-    return max_value
+    if a_dictionary:
+        max_key = max(a_dictionary, key=a_dictionary.get)
+        return max_key
+    else:
+        None
