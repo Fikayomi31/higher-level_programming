68d4a8ea96516c664f228e4ac4b81ab350685fea
Fikayomi31 <fikayoogundijo@gmail.com>
2023-12-18 22:57:05 +0000

    task 7

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,8 +11,7 @@ if __name__ == "__main__":
     database = sys.argv[3]
 
     db = MySQLdb.connect(host='localhost', user=username,
-                         password='password', database=database, port=3306)
-
+                         password=password, database=database, port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -3,10 +3,14 @@
 """
 import sys
 from model_state import Base, State
-
+from urllib.parse import quote_plus
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    """password contain special char which use been encode to url"""
+    password = 'Fikayomi31@'
+    encoded_pwd = quote_plus(password)
+     # engine = create_engine('mysql://root:Fikayomi31@.@localhost/hbtn_0e_6_usa')
 
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], encoded_pwd, sys.argv[2]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
