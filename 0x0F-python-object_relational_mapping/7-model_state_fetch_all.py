40904f64244b01139be1efc0b78d45d1036f8ef0
Fikayomi31 <fikayoogundijo@gmail.com>
2023-12-18 23:12:25 +0000

    task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from urllib.parse import quote_plus
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """password contain special char which use been encode to url"""
+    password = 'Fikayomi31@'
+    encoded_pwd = quote_plus(password)
+     # engine = create_engine('mysql://root:Fikayomi31@.@localhost/hbtn_0e_6_usa')
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], encoded_pwd, sys.argv[2]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
